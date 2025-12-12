44ad0bbcabf106aaab89e7a761dda92085c71fff
pauldatcom <paul.bajodek@gmail.com>
2025-12-12 11:09:00 +0100

    Add TP2 exercice 3: React component testing with Vitest

---
+++ tp2/exercice3/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h2 style={{ color: "green" }}>Hello world</h2>
+    </div>
+  );
+}
